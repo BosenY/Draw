14dc216bf44435804a5a799a60dcdc6976e412d5
boseny <yxy43445@gmail.com>
2018-05-11 12:47:06 +0800

    delete unless file

--- example/esmodule.js
+++
@@ -1,6 +0,0 @@
-import Draw from '../node_modules/draw-something/main.js'
-
-const flame = window.document.getElementById('flame')
-const draw1 = new Draw()
-const draw2 = new Draw({ parent: flame, maxNum: 2, iconText: '✨' })
-const draw3 = new Draw({ maxNum: 10, iconText: '❤️' })
